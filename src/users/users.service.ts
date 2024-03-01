aaff3ad0f774f289ad4b98a1601008a09e5b532a
Rafael Vasconcelos <rvascons@gmail.com>
2024-03-01 11:05:30 -0300

    Remove unused import

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
